f4e488e5c60d6fd12cd46aede78abe2edccff804
澤内涼太 <ryota0922gks@gmail.com>
2025-03-23 15:40:07 +0900

    【更新】MainAcrtivity

--- app/src/main/java/com/example/tutorial_application/MainActivity.kt
+++ app/src/main/java/com/example/tutorial_application/MainActivity.kt
@@ -50,7 +50,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello test $name!",
         modifier = modifier
     )
 }
