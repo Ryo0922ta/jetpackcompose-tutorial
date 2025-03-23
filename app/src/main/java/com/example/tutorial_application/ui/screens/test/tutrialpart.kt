ef50b365397bfb73b4dbabafcfd9af7e0aaea45e
澤内涼太 <ryota0922gks@gmail.com>
2025-03-23 17:55:50 +0900

    【更新】kotlinのpaddingの仕様はwebのmarginの仕様で画面表示される？

--- app/src/main/java/com/example/tutorial_application/ui/screens/test/tutrialpart.kt
+++ app/src/main/java/com/example/tutorial_application/ui/screens/test/tutrialpart.kt
@@ -2,6 +2,8 @@ package com.example.tutorial_application.ui.screens.test
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,23 +14,24 @@ import com.example.tutorial_application.ui.theme.Tutorial_applicationTheme
 
 @Composable
 fun TestFunc(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Test Func $name!",
-        modifier = modifier//デフォルト値を渡すことでmodifierを指定しなくても動作する
-    )
+    Surface(color = MaterialTheme.colorScheme.primary)
+    {
+        Text(
+            text = "Hello Test Func $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 //modifier　見た目や振る舞いを変更するためのオブジェクト
 //UIの定義でパラメータとして渡してあげることでこの関数の呼び出し元でもレイアウトや装飾をカスタムできる
 @Preview(
-    showBackground = true,
-    showSystemUi = true
+//    showBackground = true,
+//    showSystemUi = true
 )
 @Composable
 fun TestPreview() {
     Tutorial_applicationTheme {
-        TestFunc("tutrial",
-            modifier = Modifier.padding(16.dp).background(Color.Yellow)
-        )
+        TestFunc("tutrial")
     }
 }
