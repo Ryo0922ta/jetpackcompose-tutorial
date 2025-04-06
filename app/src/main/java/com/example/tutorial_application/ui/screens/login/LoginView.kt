5b6ba7b2e67ce9411b7a95f3b02a1203d9816526
澤内涼太 <ryota0922gks@gmail.com>
2025-04-07 03:15:56 +0900

    更新：ファイルの位置、todo実装フロント部分のみ作成、次回：Roomを使ったDB操作実装予定

--- app/src/main/java/com/example/tutorial_application/ui/screens/login/LoginView.kt
+++ app/src/main/java/com/example/tutorial_application/ui/screens/login/LoginView.kt
@@ -1,4 +1,4 @@
-package com.example.tutorial_application.ui.screens.home
+package com.example.tutorial_application.ui.screens.login
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
--- app/src/main/java/com/example/tutorial_application/ui/screens/test/MyLibraryView.kt
+++ app/src/main/java/com/example/tutorial_application/ui/screens/test/MyLibraryView.kt
@@ -1,4 +1,4 @@
-package com.example.tutorial_application.ui.screens.home
+package com.example.tutorial_application.ui.screens.test
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
